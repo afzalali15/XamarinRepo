774cac4b3cce233272080cf103f98e4c8f2913a6
Afzal Ali <afzalali15@gmail.com>
2016-07-08 11:34:02 +0530

    added property

--- XamarinHive/XamarinHive/CustomControl.cs
+++ XamarinHive/XamarinHive/CustomControl.cs
@@ -3,22 +3,24 @@ using Xamarin.Forms;
 
 namespace XamarinHive
 {
-	public class XButton : Button
-	{
-		public string BackgroundSource
-		{
-			get;
-			set;
-		}
-	}
+    public class XButton : Button
+    {
+        public string BackgroundSource {
+            get;
+            set;
+        }
+    }
 
-	public class XCheckBox : View
-	{
-		public string Text
-		{
-			get;
-			set;
-		}
-	}
+    public class XCheckBox : View
+    {
+        public bool IsChecked {
+            get;
+            set;
+        }
+        public string Text {
+            get;
+            set;
+        }
+    }
 }
 
